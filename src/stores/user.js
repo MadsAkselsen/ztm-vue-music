21e577e012a2eef5ae30a7c961a8ea07b4de6c69
Mads Akselsen <akselsenmads@gmail.com>
2024-05-23 12:43:30 +0200

    test

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,7 +1,24 @@
+import { auth, usersCollection } from '@/includes/firebase'
 import { defineStore } from 'pinia'
 
 export default defineStore('user', {
   state: () => ({
     userLoggedIn: false
-  })
+  }),
+  actions: {
+    async register(values) {
+      const userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
+
+      await usersCollection.doc(userCred.user.uid).set({
+        name: values.name,
+        email: values.email,
+        age: values.age,
+        country: values.country
+      })
+
+      await userCred.user.updateProfile({
+        displayName: values.name
+      })
+    }
+  }
 })
