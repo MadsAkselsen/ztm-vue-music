7c40dc2f3ffdfda8c2d25aa367cad2414fed8ec9
Mads Akselsen <akselsenmads@gmail.com>
2024-05-23 22:26:27 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
+import ManageView from '@/views/ManageView.vue'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: HomeView
   },
   {
+    name: 'about',
     path: '/about',
     component: AboutView
+  },
+  {
+    name: 'manage',
+    // alias works like the below commented code: it will redirect to /manage-music
+    // But the below commented code might be prefered because it might be better for SEO
+    alias: '/manage',
+    path: '/manage-music',
+    component: ManageView
+  },
+  // {
+  //   path: '/manage',
+  //   redirect: { name: 'manage' }
+  // },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
